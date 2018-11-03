6a9486033fa47d740a1b937891dfe3dd0b4339f7
Albert Fung <albert.fung@live.com>
2018-11-03 02:29:07 -0400

    Completed Routing for Navbar and misc cleaning

---
+++ src/Webpage/Landing-Page/Home.js
@@ -0,0 +1,139 @@
+import React from 'react';
+import Slider from "react-slick";
+import 'slick-carousel/slick/slick-theme.css';
+import 'slick-carousel/slick/slick.css';
+import "./Hero.css";
+import { fadeInDown ,fadeIn } from 'react-animations';
+import Radium, {StyleRoot} from 'radium';
+/* TODO 
+  - Add animation to background and stutter so carosuel comes first. (Debating?)
+*/
+const styles = {
+  /*Animation for paragraph over carosuel */
+    heroSubtitleAnimation: {
+    animation: '2s',
+    animationName: Radium.keyframes(fadeIn, 'fadeIn')
+  },
+  /* Animation for stuttering about-elements */
+   about_animation_1:{
+      animation: 'x 1.5s',
+      animationName: Radium.keyframes(fadeInDown, 'fadeInDown')
+    },
+    about_animation_2:{
+      animation: 'x 2s',
+      animationName: Radium.keyframes(fadeInDown, 'fadeInDown')
+    },
+    about_animation_3:{
+      animation: 'x 2.5s',
+      animationName: Radium.keyframes(fadeInDown, 'fadeInDown')
+    }
+}
+
+export default class Landingpage extends React.Component
+{
+    render()
+    {return(
+    <div id="landing-page">
+        <div id="hero">
+          {/* Caption over carosuel & animation container */}
+          <StyleRoot>
+            {/* Animation */}
+          <div style={styles.heroSubtitleAnimation} id="caption-container">
+            <h1>HELLO</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur 
+              adipiscing elit. Nulla dignissim arcu sed 
+              volutpat aliquet. Phasellus eget mi ac orci 
+              malesuada faucibus. Morbi congue tortor eu 
+              augue posuere, nec cursus velit vestibulum. 
+              Nunc condimentum nulla in nisl eleifend, a </p>
+          </div>
+          </StyleRoot>
+          <HeroSlider></HeroSlider>
+            {/* About me section */}
+        <div id="about-container">
+        {/* About me element-1 */}
+        <StyleRoot>
+           {/* Animation */}
+          <div style={styles.about_animation_1} className="about-elements">
+            <div className="icon-container">
+              <span className="icon fa fa-user-circle fa-4x"></span>
+            </div>
+            <h1>About Us!</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur 
+              adipiscing elit. Nulla dignissim arcu sed 
+              volutpat aliquet. Phasellus eget mi ac orci 
+              malesuada faucibus. Morbi congue tortor eu 
+              augue posuere, nec cursus velit vestibulum. 
+              Nunc condimentum nulla in nisl eleifend, a 
+            </p>
+          </div>
+          </StyleRoot>
+           {/* About me element-2 */}
+          <StyleRoot>
+             {/* Animation */}
+          <div style={styles.about_animation_2}  className="about-elements">
+            <div className="icon-container">
+              <span className="icon fas fa-archive fa-4x"></span>
+            </div>
+            <h1>Our Product.</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur 
+              adipiscing elit. Nulla dignissim arcu sed 
+              volutpat aliquet. Phasellus eget mi ac orci 
+              malesuada faucibus. Morbi congue tortor eu 
+              augue posuere, nec cursus velit vestibulum. 
+              Nunc condimentum nulla in nisl eleifend, a 
+            </p>
+          </div>
+          </StyleRoot>
+           {/* About me element-3 */}
+           <StyleRoot>
+          <div style={styles.about_animation_3} className="about-elements">
+            <div className="icon-container">
+              <span className="icon fas fa-question fa-4x"></span>
+            </div>
+            <h1>How we can help you?</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur 
+              adipiscing elit. Nulla dignissim arcu sed 
+              volutpat aliquet. Phasellus eget mi ac orci 
+              malesuada faucibus. Morbi congue tortor eu 
+              augue posuere, nec cursus velit vestibulum. 
+              Nunc condimentum nulla in nisl eleifend, a 
+            </p>
+          </div>
+           </StyleRoot>
+          </div>
+        </div>  
+    </div>
+    )}
+}
+
+class HeroSlider extends React.Component {
+    render() {
+      var settings = {
+        arrows:false,
+        infinite: true,
+        speed: 500,
+        autoplay:true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        useTransform: false,
+      };
+      return (
+        <Slider  {...settings}>
+          <div className="carosuel-container slide-1">
+            <div className="gray-layer"></div>
+          </div>
+          <div className="carosuel-container slide-2">
+            <div className="gray-layer"></div>
+          </div>
+          <div className="carosuel-container slide-3">
+            <div className="gray-layer"></div>
+          </div>
+          <div className="carosuel-container slide-4">
+            <div className="gray-layer"></div>
+          </div>
+        </Slider>
+      );}}
--- src/Webpage/Webpage.js
+++ src/Webpage/Webpage.js
@@ -1,141 +1,35 @@
 import React from 'react';
-import Slider from "react-slick";
-import HiveGraphs from '../HiveGraphs/HiveGraphs.js'
-import NavBar from "../NavBar/NavBar";
-import 'slick-carousel/slick/slick-theme.css';
-import 'slick-carousel/slick/slick.css';
-import "./Hero.css"
-import { fadeInDown ,fadeIn } from 'react-animations';
-import Radium, {StyleRoot} from 'radium';
-
-
-const styles = {
-    heroSubtitleAnimation: {
-    animation: '2s',
-    animationName: Radium.keyframes(fadeIn, 'fadeIn')
-  },
-   about_animation_1:{
-      animation: 'x 1.5s',
-      animationName: Radium.keyframes(fadeInDown, 'fadeInDown')
-    },
-    about_animation_2:{
-      animation: 'x 2s',
-      animationName: Radium.keyframes(fadeInDown, 'fadeInDown')
-    },
-    about_animation_3:{
-      animation: 'x 2.5s',
-      animationName: Radium.keyframes(fadeInDown, 'fadeInDown')
-    }
-
-}
+import HiveGraphs from '../HiveGraphs/HiveGraphs.js';
+import './NavBar.css';
+import LandingPage from "./Landing-Page/Home.js";
+import {BrowserRouter as Router, Link,Route} from 'react-router-dom';
 
 export default class Webpage extends React.Component
 {render()
     {return(
       <div id="front-page">
-       <NavBar/>
-        <div id="hero">
-          {/* Caption over carosuel */}
-          <StyleRoot>
-          <div style={styles.heroSubtitleAnimation} id="caption-container">
-            <h1>HELLO</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur 
-              adipiscing elit. Nulla dignissim arcu sed 
-              volutpat aliquet. Phasellus eget mi ac orci 
-              malesuada faucibus. Morbi congue tortor eu 
-              augue posuere, nec cursus velit vestibulum. 
-              Nunc condimentum nulla in nisl eleifend, a </p>
-          </div>
-          </StyleRoot>
-            <HeroSlider></HeroSlider>
+      <Router>
+        <div id="Router-container">
+        {/*Nav-bar using Routers to create Single page application*/}
+         <nav className="nav-bar">
+            <h1 className="text-center"><span className="black">Project </span><span className="orange">Bee</span></h1>
+            <ul className="text-right">
+              <li><Link class="nav-element" to="/"><span><i className="fas fa-home"/>Home</span></Link></li>
+              <li><Link class="nav-element" to="/My-Hive"><span><i className="fas fa-flask"/>My Hives</span></Link></li>
+              <li><Link class="nav-element" to="/Public-Data"><span><i className="fas fa-users"/>Public Data</span></Link></li>
+              <li><Link class="nav-element" to="/Open-Source"><span><i className="fas fa-code"/>Open Source</span></Link></li>
+            </ul>
+          </nav>
+        {/*"Href" that above link routers connect to TODO complete three other pages and connect them */}
+          <Route path="/" exact={true} render={()=><LandingPage></LandingPage>}></Route>  
+          <Route path="/My-Hive" render={()=><h1>RESVERED FOR MY-HIVE PAGE</h1>}></Route>
+          <Route path="/Public-Data" render={()=><h1>RESERVERED FOR PUBLIC DATA PAGE</h1>}></Route>  
+          <Route path="/Open-Source" render={()=><h1>RESERVERED FOR OPEN SOURCE PAGE</h1>}></Route>    
         </div>
-        {/* About me section */}
-        <div id="about-container">
-        {/* About me element-1 */}
-        <StyleRoot>
-          <div style={styles.about_animation_1} className="about-elements">
-            <div className="icon-container">
-              <span className="icon fa fa-user-circle fa-4x"></span>
-            </div>
-            <h1>About Us!</h1>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur 
-              adipiscing elit. Nulla dignissim arcu sed 
-              volutpat aliquet. Phasellus eget mi ac orci 
-              malesuada faucibus. Morbi congue tortor eu 
-              augue posuere, nec cursus velit vestibulum. 
-              Nunc condimentum nulla in nisl eleifend, a 
-            </p>
-          </div>
-          </StyleRoot>
-           {/* About me element-2 */}
-          <StyleRoot>
-          <div style={styles.about_animation_2}  className="about-elements">
-            <div className="icon-container">
-              <span className="icon fas fa-archive fa-4x"></span>
-            </div>
-            <h1>Our Product.</h1>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur 
-              adipiscing elit. Nulla dignissim arcu sed 
-              volutpat aliquet. Phasellus eget mi ac orci 
-              malesuada faucibus. Morbi congue tortor eu 
-              augue posuere, nec cursus velit vestibulum. 
-              Nunc condimentum nulla in nisl eleifend, a 
-            </p>
-          </div>
-          </StyleRoot>
-           {/* About me element-3 */}
-           <StyleRoot>
-          <div style={styles.about_animation_3} className="about-elements">
-            <div className="icon-container">
-              <span className="icon fas fa-question fa-4x"></span>
-            </div>
-            <h1>How we can help you?</h1>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur 
-              adipiscing elit. Nulla dignissim arcu sed 
-              volutpat aliquet. Phasellus eget mi ac orci 
-              malesuada faucibus. Morbi congue tortor eu 
-              augue posuere, nec cursus velit vestibulum. 
-              Nunc condimentum nulla in nisl eleifend, a 
-            </p>
-          </div>
-          </StyleRoot>
-        </div>
-        
-        {/* Will re-add later <HiveGraphs/>*/}
-      </div>
-        );
-    }
-};
+      </Router>
 
-class HeroSlider extends React.Component {
-  render() {
-    var settings = {
-      arrows:false,
-      infinite: true,
-      speed: 500,
-      autoplay:true,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      useTransform: false,
-    };
-    return (
-      <Slider  {...settings}>
-        <div className="carosuel-container slide-1">
-          <div className="gray-layer"></div>
-        </div>
-        <div className="carosuel-container slide-2">
-          <div className="gray-layer"></div>
-        </div>
-        <div className="carosuel-container slide-3">
-          <div className="gray-layer"></div>
-        </div>
-        <div className="carosuel-container slide-4">
-          <div className="gray-layer"></div>
-        </div>
-      </Slider>
+      </div>
     );
   }
 }
+
