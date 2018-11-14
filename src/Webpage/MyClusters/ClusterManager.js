32c67ba2d7b49752e5ff92c59079f3119eb63a92
mahnoorcheema <mchee050@uottawa.ca>
2018-11-14 18:05:12 -0500

    Adds hive data passing and resolves login on refresh issue

--- src/Webpage/MyClusters/ClusterManager.js
+++ src/Webpage/MyClusters/ClusterManager.js
@@ -1,9 +1,42 @@
 import React from "react";
 
-export default class ClusterManager extends React.Component {
+
+class HiveBadge extends React.Component {
   render() {
-    return (<div>
-      {JSON.stringify(this.props.cluster)}
-    </div>);
+    return (
+      <p className={this.props.public ? "public" : "private"} >
+        {this.props.name}
+      </p>);
+  }
+}
+
+
+
+export default class ClusterManager extends React.Component {
+
+  static renderHiveList(hives) {
+    return Object.entries(hives)
+      .map(([id, hive]) =>
+        (<HiveBadge key={id} {...hive}/>));
+  }
+
+  render() {
+    if (this.props) {
+      return (<div>
+        <h2>{this.props.name}</h2>
+        <div>
+          <h3>Hives:</h3>
+          <ul>
+            {ClusterManager.renderHiveList(this.props.hives)}
+          </ul>
+          <h3>Owners:</h3>
+          <ul>
+
+          </ul>
+        </div>
+      </div>);
+    } else {
+      return (<p>Error</p>)
+    }
   }
 }
--- src/Webpage/MyClusters/MyClusters.js
+++ src/Webpage/MyClusters/MyClusters.js
@@ -13,21 +13,21 @@ export default class MyClusters extends React.Component {
     return {id: snap.id, ...snap.data()};
   }
 
-  async componentDidMount() {
-    if (auth.currentUser) {
-      this.setState({loggedIn: true});
-      const userId = auth.currentUser.uid;
-      console.log("UserId", userId);
-      if (userId) {
-        const queryResult = await firestore.collection("cluster")
-          .where("owners", "array-contains", userId)
-          .get();
-        const clusters = queryResult.docs.map(MyClusters.snapToCluster);
-        this.setState({clusters});
+  componentDidMount() {
+    auth.onAuthStateChanged(async user => {
+      if (user) {
+        this.setState({loggedIn: true});
+        if (user.uid) {
+          const queryResult = await firestore.collection("cluster")
+            .where("owners", "array-contains", user.uid)
+            .get();
+          const clusters = queryResult.docs.map(MyClusters.snapToCluster);
+          this.setState({clusters});
+        }
+      } else {
+        this.setState({loggedIn: false})
       }
-    } else {
-      this.setState({loggedIn: false})
-    }
+    });
   }
 
 
@@ -37,14 +37,14 @@ export default class MyClusters extends React.Component {
       return (
         <main>
           <div>
-            {this.state.clusters.map(cluster => <ClusterManager key={cluster.id} cluster={cluster}/>)}
+            {this.state.clusters.map(cluster => <ClusterManager key={cluster.id} {...cluster}/>)}
           </div>
           <button>Add Cluster</button>
         </main>
       );
     } else {
       return (<main>
-        Please login to view your clusters
+        loading
       </main>);
     }
   }
