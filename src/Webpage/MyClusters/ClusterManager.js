21cead9120345ec25b315320a23df853fc2bb2a2
mahnoorcheema <mchee050@uottawa.ca>
2018-11-14 22:54:41 -0500

    Adds owner fetching and formatting to cluster manager

--- src/Webpage/MyClusters/ClusterManager.js
+++ src/Webpage/MyClusters/ClusterManager.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {firestore} from "../../Firebase";
 
 
 class HiveBadge extends React.Component {
@@ -14,10 +15,40 @@ class HiveBadge extends React.Component {
 
 export default class ClusterManager extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {users: {}};
+  }
+
+
   static renderHiveList(hives) {
     return Object.entries(hives)
       .map(([id, hive]) =>
-        (<HiveBadge key={id} {...hive}/>));
+        (<li key={id}><HiveBadge {...hive}/></li>));
+  }
+
+
+
+  formatOwner(ownerId) {
+    if (this.state.users[ownerId]) {
+      const user = this.state.users[ownerId];
+      return `${user.name}(${user.email})`;
+    }
+  }
+
+  componentDidMount() {
+    this.props.owners.forEach(async ownerId => {
+      const queryResult = await firestore.collection("users")
+        .doc(ownerId)
+        .get();
+      const user = queryResult.data();
+      this.setState({
+        users: {
+          [ownerId]: user,
+          ...this.state.users
+        }
+      });
+    });
   }
 
   render() {
@@ -28,10 +59,19 @@ export default class ClusterManager extends React.Component {
           <h3>Hives:</h3>
           <ul>
             {ClusterManager.renderHiveList(this.props.hives)}
+            <li>
+              <button>Add Hive</button>
+            </li>
           </ul>
           <h3>Owners:</h3>
           <ul>
-
+            {this.props.owners.map(ownerId => (
+              <li key={ownerId}>
+                {this.formatOwner(ownerId)}
+              </li>))}
+            <li>
+              <button>Add Owner</button>
+            </li>
           </ul>
         </div>
       </div>);
