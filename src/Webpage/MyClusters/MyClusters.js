7661bcc16d007926cbf5f8df1bc37e06900cae00
mahnoorcheema <mchee050@uottawa.ca>
2018-11-15 21:37:25 -0500

    Adds cluster adding functionality

--- src/Webpage/MyClusters/MyClusters.js
+++ src/Webpage/MyClusters/MyClusters.js
@@ -1,10 +1,26 @@
 import React from "react";
 import ClusterManager from "./ClusterManager";
 import "./MyClusters.css";
+import SingleInputForm from "../../Shared/SingleInputForm";
+import {firestore, auth} from "../../Firebase";
 
 export default class MyClusters extends React.Component {
   constructor(props) {
     super(props);
+    // this.addCluster = this.addCluster.bind(this);
+  }
+
+  static async addCluster(name) {
+    const user = auth.currentUser;
+    if (!user) {
+      throw new Error("Must be logged in to create clusters");
+    }
+    const cluster = await firestore.collection("cluster").add({
+      name,
+      owners: [user.email],
+      hives: []
+    });
+    await firestore.collection("measurements").doc(cluster.id).set({});
   }
 
   render() {
@@ -13,10 +29,12 @@ export default class MyClusters extends React.Component {
         <div className="container">
           {this.props.clusters.map(cluster => <ClusterManager key={cluster.id} {...cluster}/>)}
         </div>
-        <hr/>
         <div className="row-center">
-          <button className="btn">+ Add Cluster</button>
+          <SingleInputForm label="+ Add a Cluster" onSubmit={MyClusters.addCluster}>
+            <input type="text" maxLength="100"/>
+          </SingleInputForm>
         </div>
+        <hr/>
       </main>
     );
   }
