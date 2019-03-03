c30575d04c908f5cff1450bbf28fedc484ef6a77
mahnoorcheema <mchee050@uottawa.ca>
2019-03-03 14:29:43 -0500

    Fixes dates on graph and orders by most recent measurement

--- src/Webpage/MyHives/MyHives.js
+++ src/Webpage/MyHives/MyHives.js
@@ -5,6 +5,9 @@ import RecentMeasurements from "./RecentMeasurements";
 import HiveGraph from "./HiveGraph";
 import {firestore} from "../../Firebase";
 
+const dateTimeOptions = {month: "numeric", day: "numeric" , hour: "numeric", minute: "2-digit"};
+const DateFormatter = new Intl.DateTimeFormat("en-GB", dateTimeOptions);
+
 export default class MyHives extends Component {
   constructor(props) {
     super(props);
@@ -23,7 +26,7 @@ export default class MyHives extends Component {
     const measurements = snapshot.docs.map(snap => ({
       id: snap.id,
       ...snap.data()
-    }));
+    })).reverse();
     const times = measurements.map(measurement => MyHives.formatDate(measurement.date));
     this.setState({measurements, times});
   }
@@ -35,14 +38,14 @@ export default class MyHives extends Component {
       .collection("hives")
       .doc(selectedHive)
       .collection("measurements")
-      .orderBy("date")
+      .orderBy("date", "desc")
       .limit(31 * 24 * 4)
       .onSnapshot(this.measurementsUpdated);
   }
 
   static formatDate(firestoreDate) {
-    const date = new Date(firestoreDate.seconds);
-    return `${date.getHours()}:${date.getMinutes()}`;
+    const date = new Date(firestoreDate.seconds * 1000);
+    return DateFormatter.format(date);
   }
 
   getRecentMeasurements() {
